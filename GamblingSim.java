b8ab6732216ad01eb651771c694354194b784a6b
Karthik <jeefkarthik036@gmail.com>
2022-03-31 22:14:22 +0530

    added-UC1

---
+++ GamblingSim.java
@@ -0,0 +1,12 @@
+package com.Bridge;
+
+public class GamblingSim {
+	static final int TOTAL_AMT=100;
+	static final int BET_AMT=1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gambling game");
+	}
+
+}
