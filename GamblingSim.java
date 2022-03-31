64bf0fbaaf8bbad2b2455800284afd9209ba337b
Karthik <jeefkarthik036@gmail.com>
2022-03-31 23:09:44 +0530

    added-UC4

--- GamblingSim.java
+++ GamblingSim.java
@@ -1,32 +1,38 @@
 package com.Bridge;
 
-
 //UC1 Initializing stake and betting amount
 public class GamblingSim {
 	static final int TOTAL_AMT = 100;
 	static final int BET_AMT = 1;
+	static final int DAYSINMONTH = 20;
 
 	public static void game() {
 
 		int result;
-		result = TOTAL_AMT;
+		int totalamount = 0;
+//UC4 Calculate total amount for 20 days
+		for (int day = 1; day < DAYSINMONTH; day++) {
+			result = TOTAL_AMT;
 //UC3 resign for day if stack reaches more of less than 50%
-		while( result > (TOTAL_AMT/2) && result < (TOTAL_AMT+(TOTAL_AMT/2)) )
-		{
-			double game_check = Math.floor(Math.random()*10)%2;
+			while (result > (TOTAL_AMT / 2) && result < (TOTAL_AMT + (TOTAL_AMT / 2))) {
+				double game_check = Math.floor(Math.random() * 10) % 2;
 //UC2 Betting $1 so either wins or lose
-			if( game_check == 1 )
-			{
-				result = result + BET_AMT ;
-				System.out.println("won "+ result);
+				if (game_check == 1) {
+					result = result + BET_AMT;
+					//System.out.println("won " + result);
+				} else {
+					result = result - BET_AMT;
+					//System.out.println("lost " + result);
+				}
 			}
-			else
-			{
-				result = result - BET_AMT ;
-				System.out.println("lost "+result);
+			if (result > TOTAL_AMT) {
+				System.out.println("Result at end of day :" + day + " is  Gambler Won $" + result);
+			} else {
+				System.out.println("Result at end of day :" + day + " is Gambler lost $" + result);
 			}
+			totalamount = totalamount + result;
 		}
-		System.out.println("Result at the end of day :"+result);
+		System.out.println("Total Amount after 20 days " + totalamount);
 	}
 
 	public static void main(String[] args) {
